3180259a1dfb70eee4aa0169ad8551a99ecfd49f
jamesiweobi <60060852+jamesiweobi@users.noreply.github.com>
2021-05-09 03:01:19 +0100

    Added the show item container functionality

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const alert = document.querySelector('.alert');
 const form = document.querySelector('.grocery-form');
 const submutBtn = document.querySelector('.submit-btn');
 const container = document.querySelector('.grocery-container');
-const list = document.querySelector('grocery-list');
+const list = document.querySelector('.grocery-list');
 const clearBtn = document.querySelector('.clear-btn');
 const grocery = document.getElementById('grocery');
 
@@ -25,7 +25,27 @@ function addItem(e) {
   const value = grocery.value;
   const id = new Date().getTime().toString();
   if (value && !editFlag) {
-    console.log('adding to the list');
+    // create element ad add article class
+    const element = document.createElement('article');
+    element.classList.add('grocery-item');
+    // create attribute and add it to the created element
+    const attr = document.createAttribute('data-id');
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = ` <p class="title">${value}</p>
+           <div class="btn-container">
+             <button type="button" class="edit-btn">
+               <i class="fas fa-edit"></i>
+             </button>
+             <button type="button" class="delete-btn">
+               <i class="fas fa-trash"></i>
+             </button>
+           </div>`;
+    //  appendChild
+    list.appendChild(element);
+    displayAlert('item added to the list', 'success');
+    // show container
+    container.classList.add('show-container');
   } else if (value && editFlag) {
     console.log('editting');
   } else {
